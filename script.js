2527ed31c5ff3c589af945d685d577e2c119765a
nny18 <s.ricoj@uniandes.edu.co>
2025-11-06 18:29:02 +0000

    basico html

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const titulo = document.getElementById("titulo");
+const textoInicial = document.getElementById("textoInicial");
+let estadoInicial = true;
+let posicionTexto = 0;
+const  textos = ["Hola", "como", "estas", ":)", "y", "chau"];
 
-titulo.addEventListener("click", ()=> {
-    titulo.innerText = "Holiii :)";
+
+textoInicial.innerText = textos[posicionTexto];
+
+textoInicial.addEventListener("click", () => {
+    if (posicionTexto < textos.length - 1) {
+    posicionTexto = posicionTexto + 1;
+    textoInicial.innerText = textos[posicionTexto];
+    } else {
+        posicionTexto = 0;
+        textoInicial.innerText = textos[posicionTexto];
+    }
 })
+
+titulo.addEventListener("click", () => {
+    if (estadoInicial === true) {
+        titulo.innerText = "Holiii :)";
+    } else {
+        titulo.innerText = "¿Avatar? ¡Avatar!";
+    }
+    estadoInicial = !estadoInicial;
+}) 
